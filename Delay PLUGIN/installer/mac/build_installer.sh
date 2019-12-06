e0f80a56bf011308d8174bffda842c990b615e89
Quin Scacheri <qs366@nyu.edu>
2019-12-06 11:14:44 -0500

    finished mac installer

--- Delay PLUGIN/installer/mac/build_installer.sh
+++ Delay PLUGIN/installer/mac/build_installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xcodebuild -project ../Builds/MacOSX/Delay\ PLUGIN.xcodeproj -configuration Release -scheme "Delay PLUGIN - All"
+xcodebuild -project ../../Builds/MacOSX/Delay\ PLUGIN.xcodeproj -configuration Release -scheme "Delay PLUGIN - All"
 packagesbuild Delay\ Plugin.pkgproj
 dmgcanvas Delay.dmgCanvas DyanDelay.dmg
 
